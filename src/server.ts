c055c271f69c193b8d6e4aacbd6c7b5ff852e25a
gedira27 <20030013@itcelaya.edu.mx>
2024-12-02 17:33:39 -0600

    357 Separando rutas

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,9 @@
 import express from 'express'
+import router from './router'
 
 const server = express()
 
+server.use('/api/products', router)
 
-//Routing 
-server.get('/', (req, res) => {
-    res.json('Desde GET')
-})
-
-server.post('/', (req, res) => {
-    res.json('Desde POST')
-})
-
-server.put('/', (req, res) => {
-    res.json('Desde PUT')
-})
-
-server.patch('/', (req, res) => {
-    res.json('Desde PATCH')
-})
-
-server.delete('/', (req, res) => {
-    res.json('Desde DELETE')
-})
 
 export default server
