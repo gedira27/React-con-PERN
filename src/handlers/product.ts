1f0f59bc67006e04271f4d8baf848bf35ca254a3
gedira27 <20030013@itcelaya.edu.mx>
2024-12-03 01:17:05 -0600

    374 Creando el Endpoint para eliminar

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -70,3 +70,16 @@ export const updateAvailability= async (req: Request, res: Response) => {
     await product.save()
     res.json({data: product})
 }
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    const {id} = req.params
+    const product = await Product.findByPk(id)
+
+    if(!product){
+        res.status(404).json({error: 'Producto no encontrado'})
+        return 
+    }
+
+    await product.destroy()
+    res.json({data: 'Producto Eliminado'})
+}
