922be3cb1828876698fdbfa7a811b131ac2e89e3
gedira27 <20030013@itcelaya.edu.mx>
2024-12-02 23:39:53 -0600

    367 Validando Crear Producto - Pero no sirve

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,9 +1,26 @@
-import {Request, Response} from 'express'
+import {Request, Response } from 'express'
+import {check, validationResult } from 'express-validator'
 import Product from '../models/Product.model'
 
 export const createProduct = async (req: Request, res: Response) => {
 
-    const product = await Product.create(req.body)
+    //Validación
+    /*await check('name')
+            .notEmpty().withMessage('El nombre del Producto no puede ir vacío')
+            .run(req)
+    await check('price')
+            .isNumeric().withMessage('Valor no válido')
+            .notEmpty().withMessage('El precio del Producto no puede ir vacío')
+            .custom(value => value >0).withMessage('Precio no válido')
+            .run(req)
 
+
+    let errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+    */
+
+    const product = await Product.create(req.body)
     res.json({data: product})
 }
